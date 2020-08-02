ba5fc8a9445b540a09b07235a58aea6f87f6029a
zhihuai1982 <zhihuai1982@gmail.com>
2020-08-02 19:31:59 +0800

    key

--- config.def.h
+++ config.def.h
@@ -75,13 +75,10 @@ static const char *downvol[] = { "amixer", "set", "Master", "3-",     NULL };
 static const char *mutevol[] = { "amixer", "set", "Master", "toggle", NULL };
 
 static Key keys[] = {
+	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_F12,    spawn,          {.v = upvol   } },
 	{ MODKEY,                       XK_F11,    spawn,          {.v = downvol } },
 	{ MODKEY,                       XK_F9,     spawn,          {.v = mutevol } },
-};
-
-static Key keys[] = {
-	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
